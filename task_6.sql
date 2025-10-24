8bb6e450b52a9104a5466192caf01fca0cb5e44a
Ian Mwangi <marizebruno@gmail.com>
2025-10-24 12:57:05 -0700

    Fix task_6.sql: insert multiple rows into customer table

--- task_6.sql
+++ task_6.sql
@@ -1,13 +1,5 @@
-USE alx_book_store;
-
--- Insert three customers into Customers (plural)
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-  (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-  (3, 'Obed Ehoneah',   'eobed@sandtech.com',  '125 Happiness Ave.'),
-  (4, 'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.');
-
--- Also insert same rows into customer (singular) in case the grader expects that table name
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-  (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-  (3, 'Obed Ehoneah',   'eobed@sandtech.com',  '125 Happiness Ave.'),
-  (4, 'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
